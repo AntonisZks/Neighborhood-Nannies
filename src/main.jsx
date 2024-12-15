2d36ceab8ccc89489d40c39d85665d7f91a65334
AntonisZks <antoniszks2003@gmail.com>
2024-12-15 16:25:47 +0200

    Added the react-router-dom for multi-page navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Import the required modules from react and react-dom
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import the pages of the application
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
+
+// Import the global styles of the application
+import './index.css';
+
+
+// Create a browser router with the routes of the application
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <div>Custom 404 Not Found</div>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+    errorElement: <div>Custom 404 Not Found</div>,
+  }
+]);
+
+// Create the main root element of the application and render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
