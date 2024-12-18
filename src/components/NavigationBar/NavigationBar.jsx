923d46258ac2efa8a1f11ac7faf9234d4094f835
AntonisZks <antoniszks2003@gmail.com>
2024-12-18 20:43:54 +0200

    Changed the navigation bar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -4,43 +4,66 @@ export default function NavigationBar() {
   return (
     <>
       <ul className={styles.navigation_list}>
-        <li>
+        <li className={styles.dropdown_tab}>
             <div className={styles.tab_title}>
+              <i className="fa-solid fa-user"></i>
               <p>Κηδεμόνες</p>
               <i className="fa-solid fa-caret-down"></i>
             </div>
             <ul className={styles.dropdown}>
-              <li>Σύνδεση</li>
-              <li>Εγγραφή</li>
+              <li>
+                <i className="fa-solid fa-right-to-bracket"></i>
+                <p>Σύνδεση</p>
+              </li>
+              <li>
+                <i className="fa-solid fa-user-plus"></i>
+                <p>Εγγραφή</p>
+              </li>
             </ul>
         </li>
-        <li>
+        <li className={styles.dropdown_tab}>
           <div className={styles.tab_title}>
+            <i className="fa-regular fa-user"></i>
             <p>Επιμελητές</p>
             <i className="fa-solid fa-caret-down"></i>
           </div>
           <ul className={styles.dropdown}>
-            <li>Σύνδεση</li>
-            <li>Εγγραφή</li>
+            <li>
+              <i className="fa-solid fa-right-to-bracket"></i>
+              <p>Σύνδεση</p>
+            </li>
+            <li>
+              <i className="fa-solid fa-user-plus"></i>
+              <p>Εγγραφή</p>
+            </li>
           </ul>
         </li>
-        <li>
+        <li className={styles.dropdown_tab}>
           <div className={styles.tab_title}>
+            <i className="fa-solid fa-question"></i>
             <p>Οδηγίες</p>
             <i className="fa-solid fa-caret-down"></i>
           </div>
           <ul className={styles.dropdown}>
-            <li>Σύνδεση</li>
-            <li>Εγγραφή</li>
+            <li>
+              <i className="fa-solid fa-user"></i>
+              <p>Κηδεμόνες</p>
+            </li>
+            <li>
+              <i className="fa-regular fa-user"></i>
+              <p>Επιμελητές</p>
+            </li>
           </ul>
         </li>
-        <li>
+        <li className={styles.not_dropdown_tab}>
           <div className={styles.tab_title}>
+            <i className="fa-solid fa-bullhorn"></i>
             <p>Ανακοινώσεις</p>
           </div>
         </li>
-        <li>
+        <li className={styles.not_dropdown_tab}>
           <div className={styles.tab_title}>
+            <i className="fa-solid fa-comment"></i>
             <p>Επικοινωνία</p>
           </div>
         </li>
