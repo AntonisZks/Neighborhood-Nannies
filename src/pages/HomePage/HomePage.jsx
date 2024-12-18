bca84ac08788a35fee69fecb48e2f7484e270409
AntonisZks <antoniszks2003@gmail.com>
2024-12-19 01:34:59 +0200

    Changed the home main content

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,20 +6,35 @@ import styles from "./HomePage.module.css";
 import walter from "../../assets/imgs/Walter_White_S5B.png";
 import deadpool from "../../assets/imgs/Deadpool-3.avif";
 
+import nanny from "../../assets/imgs/nanny_image.jpg";
+import parents from "../../assets/imgs/parents_image.jpg";
+
 export default function HomePage() {
   return (
     <div className={styles.outer_container}>
       <Header className={styles.header} />
-      <div className={styles.main_content}>
-        <div className={styles.left_section}>
-          <h1>Νταντάδες της Γειτονιάς</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, ipsum cumque corrupti ea nisi at modi cum accusantium sequi aut numquam fugit atque ratione eaque similique fugiat porro laboriosam eligendi dignissimos quia commodi? Recusandae natus ratione nostrum eveniet voluptatem animi, sit sed aspernatur aperiam error, eius commodi. Minus, voluptates praesentium.</p>
+      <div className={styles.main_section}>
+        <div className={styles.nanny_container}>
+          <img className={styles.nanny_image} src={nanny} alt="" />
+          <h1 className={styles.title}>Ψάχνεις ενασχόληση με παιδιά;</h1>
+          <p className={styles.description}>Δημιούργησε το προφίλ σου και βρες οικογένειες που χρειάζονται την υποστήριξή σου! Είτε έχεις εμπειρία ως νταντά είτε μόλις ξεκινάς, η πλατφόρμα μας σου δίνει τη δυνατότητα να επιλέξεις ευέλικτες συνεργασίες και να κάνεις τη διαφορά στη ζωή μιας οικογένειας.</p>
+          <button>
+            <div className={styles.button_content}>
+              <p>Ξεκίνα Τώρα</p>
+              <i class="fa-solid fa-arrow-right-to-bracket"></i>
+            </div>
+          </button>
         </div>
-        <div className={styles.right_section}>
-          <div className={styles.user_options}>
-            <UserCard option_title="Κηδεμόνας" img={walter}/>
-            <UserCard option_title="Επιμελητής" img={deadpool}/>
-          </div>
+        <div className={styles.parent_container}>
+          <img className={styles.parent_image} src={parents} alt="" />
+          <h1 className={styles.title}>Ψάχνεις νταντά για το παιδί σου;</h1>
+          <p className={styles.description}>Βρες την ιδανική νταντά για τα παιδιά σου! Η πλατφόρμα μας σου προσφέρει πρόσβαση σε αξιόπιστους επαγγελματίες με εμπειρία στη φροντίδα παιδιών. Αναζήτησε εύκολα και γρήγορα, βασισμένος στις ανάγκες της οικογένειάς σου.</p>
+          <button>
+            <div className={styles.button_content}>
+              <p>Ξεκίνα Τώρα</p>
+              <i class="fa-solid fa-arrow-right-to-bracket"></i>
+            </div>
+          </button>
         </div>
       </div>
       <Footer className={styles.footer} />
